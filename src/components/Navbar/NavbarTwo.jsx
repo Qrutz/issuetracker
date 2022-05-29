6f52f4077a0b2545aa3e966cc461fd7588b5dd20
Qrutz <Johan-Sandgren@hotmail.com>
2022-05-29 16:31:42 +0200

    dropdown fix

--- src/components/Navbar/NavbarTwo.jsx
+++ src/components/Navbar/NavbarTwo.jsx
@@ -11,7 +11,7 @@ export default function NavbarTwo() {
             <nav className=' h-20 bg-slate-50 border-gray-200 px-2 flex justify-between   '>
                   {/*Create a large text in the left corner that says ISSUE TRACKER WITH A DROPDOWN NEXT to it*/}
                  <h2 className='font-mono ml-3 self-center text-5xl tracking-widetracking-wide font-bold'>ISSUE TRACKER</h2>
-                 {toggle ? <button onClick={() => setToggle(!toggle)} className='self-center mr-9 font-semibold'>Abort submission</button> : <button onClick={() => setToggle(!toggle)} className='self-center mr-9 font-semibold'>Add an issue</button>}
+                 {toggle ? <button onClick={() => setToggle(!toggle)} className='self-center mr-9 font-semibold'>Close</button> : <button onClick={() => setToggle(!toggle)} className='self-center mr-9 font-semibold'>Add an issue</button>}
                  
                   
                   
--- src/components/Navbar/dropdown.jsx
+++ src/components/Navbar/dropdown.jsx
@@ -18,11 +18,11 @@ export default function Dropdown() {
     }
 
     return (   
-        <form className='flex justify-center mt-3' onSubmit={handleSubmit}>
+        <form className='flex flex-col justify-center mt-3  items-center' onSubmit={handleSubmit}>
         <input value={ticketTitle} onChange={(e) => setTicketTitle(e.target.value)} type="text" placeholder='ticket title' required/>
         <input value={ticketDescription} onChange={(e) => setTicketDescription(e.target.value)}  type="text" placeholder='Description' required/>
         <input value={ticketUrgency} onChange={(e) => setTicketUrgency(e.target.value)} type="text" placeholder='Urgency?' required/>
-        <input value={ticketStatus} onChange={(e) => setTicketStatus(e.target.value)} type="text" placeholder='Status?' required/>   
+        <input  value={ticketStatus} onChange={(e) => setTicketStatus(e.target.value)} type="text" placeholder='Open / Closed / ...' /> 
         <button className='font-bold   ml-3' type='Submit'>Submit Issue</button>
 
     </form>
